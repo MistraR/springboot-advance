d0faede12c9465b68975575d29b5a4aa9dc74468
MistraR <mistra3030000>
2020-01-04 22:17:20 +0800

    Initial commit

---
+++ src/main/java/com/advance/mistra/SpringbootAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.advance.mistra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdvanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdvanceApplication.class, args);
+    }
+
+}
