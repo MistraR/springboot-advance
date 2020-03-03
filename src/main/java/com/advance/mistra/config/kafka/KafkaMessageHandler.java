02fc84195dbe5942c0a0a1dbc8ae2622c37e53c4
wangrui <mistra3030000>
2020-03-03 23:14:54 +0800

    @KafkaListeners

--- src/main/java/com/advance/mistra/config/kafka/KafkaMessageHandler.java
+++ src/main/java/com/advance/mistra/config/kafka/KafkaMessageHandler.java
@@ -3,6 +3,7 @@ package com.advance.mistra.config.kafka;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.KafkaListeners;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -29,17 +30,36 @@ public class KafkaMessageHandler {
         if (message.isPresent()) {
             Object msg = message.get();
             log.info("》》》》》》》》》》》》》》》》》》》》》topicTest1消费了Topic:{},Message:{}", topic, msg);
+
+
             ack.acknowledge();
         }
     }
 
-//    @KafkaListener(topics = KafkaProperties.TOPIC, groupId = KafkaProperties.GROUP_ID_2)
-//    public void topicTest2(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-//        Optional message = Optional.ofNullable(record.value());
-//        if (message.isPresent()) {
-//            Object msg = message.get();
-//            log.info("》》》》》》》》》》》》》》》》》》》》》topicTest2消费了Topic:{},Message:{}", topic, msg);
-//            ack.acknowledge();
-//        }
-//    }
+    @KafkaListener(topics = KafkaProperties.TOPIC, groupId = KafkaProperties.GROUP_ID_2)
+    public void topicTest2(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        Optional message = Optional.ofNullable(record.value());
+        if (message.isPresent()) {
+            Object msg = message.get();
+            log.info("》》》》》》》》》》》》》》》》》》》》》topicTest2消费了Topic:{},Message:{}", topic, msg);
+            ack.acknowledge();
+        }
+    }
+
+    /**
+     * 消费多个topic
+     *
+     * @param record
+     * @param ack
+     * @param topic
+     */
+    @KafkaListeners({@KafkaListener(topics = "topic1"), @KafkaListener(topics = "topic2")})
+    public void batchListener(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        Optional message = Optional.ofNullable(record.value());
+        if (message.isPresent()) {
+            Object msg = message.get();
+            log.info("》》》》》》》》》》》》》》》》》》》》》topicTest1消费了Topic:{},Message:{}", topic, msg);
+            ack.acknowledge();
+        }
+    }
 }
