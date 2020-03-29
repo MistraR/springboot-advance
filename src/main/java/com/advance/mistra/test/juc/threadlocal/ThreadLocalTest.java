b03f70de04d2202fc1271a0252db6261b0f447aa
wangrui <wrmistra@gmail.com>
2020-03-29 13:45:22 +0800

    线程本地遍变量测试

---
+++ src/main/java/com/advance/mistra/test/juc/threadlocal/ThreadLocalTest.java
@@ -0,0 +1,55 @@
+package com.advance.mistra.test.juc.threadlocal;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/3/29 13:43
+ * @ Description: 线程本地遍变量测试。利用ThreadLocal，给每个线程分配自己的dateFormat对象，保证了线程安全，高效利用内存
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class ThreadLocalTest {
+
+    public static ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 1000; i++) {
+            int finalI = i;
+            threadPool.submit(new Runnable() {
+                @Override
+                public void run() {
+                    String date = new ThreadLocalTest().date(finalI);
+                    System.out.println(date);
+                }
+            });
+        }
+        threadPool.shutdown();
+    }
+
+    public String date(int seconds) {
+        //参数的单位是毫秒，从1970.1.1 00:00:00 GMT计时
+        Date date = new Date(1000 * seconds);
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat dateFormat = ThreadSafeFormatter.dateFormatThreadLocal2.get();
+        return dateFormat.format(date);
+    }
+}
+
+class ThreadSafeFormatter {
+
+    public static ThreadLocal<SimpleDateFormat> dateFormatThreadLocal = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+    };
+
+    public static ThreadLocal<SimpleDateFormat> dateFormatThreadLocal2 = ThreadLocal
+            .withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+}
