782905ba6cb409bd48c58b4f4339c70c923c4146
wangrui <wrmistra@gmail.com>
2020-04-05 15:59:42 +0800

    ReentrantReadWriteLock可以降级，不能升级

---
+++ src/main/java/com/advance/mistra/test/juc/lock/readwrite/NonfairBargeDemo.java
@@ -0,0 +1,73 @@
+package com.advance.mistra.test.juc.lock.readwrite;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/4/5 15:33
+ * @ Description: 非公平和公平的ReentrantReadWriteLock的策略
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class NonfairBargeDemo {
+
+    private static ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock(
+            true);
+
+    private static ReentrantReadWriteLock.ReadLock readLock = reentrantReadWriteLock.readLock();
+    private static ReentrantReadWriteLock.WriteLock writeLock = reentrantReadWriteLock.writeLock();
+
+    private static void read() {
+        System.out.println(Thread.currentThread().getName() + "开始尝试获取读锁");
+        readLock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "得到读锁，正在读取");
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "释放读锁");
+            readLock.unlock();
+        }
+    }
+
+    private static void write() {
+        System.out.println(Thread.currentThread().getName() + "开始尝试获取写锁");
+        writeLock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "得到写锁，正在写入");
+            try {
+                Thread.sleep(40);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "释放写锁");
+            writeLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(()->write(),"Thread1").start();
+        new Thread(()->read(),"Thread2").start();
+        new Thread(()->read(),"Thread3").start();
+        new Thread(()->write(),"Thread4").start();
+        new Thread(()->read(),"Thread5").start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Thread thread[] = new Thread[1000];
+                for (int i = 0; i < 1000; i++) {
+                    thread[i] = new Thread(() -> read(), "子线程创建的Thread" + i);
+                }
+                for (int i = 0; i < 1000; i++) {
+                    thread[i].start();
+                }
+            }
+        }).start();
+    }
+}
