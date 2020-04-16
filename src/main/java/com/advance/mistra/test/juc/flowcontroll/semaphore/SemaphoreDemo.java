49e99e3c46b2be82638b427608ba0f943d5d0c22
wangrui <wrmistra@gmail.com>
2020-04-16 23:20:04 +0800

    SemaphoreDemo

---
+++ src/main/java/com/advance/mistra/test/juc/flowcontroll/semaphore/SemaphoreDemo.java
@@ -0,0 +1,45 @@
+package com.advance.mistra.test.juc.flowcontroll.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/4/6 16:57
+ * @ Description: 演示Semaphore用法
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class SemaphoreDemo {
+
+    static Semaphore semaphore = new Semaphore(5, true);
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(50);
+        for (int i = 0; i < 5; i++) {
+            service.submit(new Task());
+        }
+        service.shutdown();
+    }
+
+    static class Task implements Runnable {
+        @Override
+        public void run() {
+            try {
+                semaphore.acquire(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "拿到了许可证");
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "释放了许可证");
+            semaphore.release(3);
+        }
+    }
+}
