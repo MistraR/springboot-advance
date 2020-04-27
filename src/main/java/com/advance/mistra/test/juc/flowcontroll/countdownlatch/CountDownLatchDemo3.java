7a9ee36334f85741ce101b07d8d5b821318ee775
wangrui <wrmistra@gmail.com>
2020-04-27 22:44:31 +0800

    Default Changelist

---
+++ src/main/java/com/advance/mistra/test/juc/flowcontroll/countdownlatch/CountDownLatchDemo3.java
@@ -0,0 +1,51 @@
+package com.advance.mistra.test.juc.flowcontroll.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/4/6 16:45
+ * @ Description: 一等多和多等一结合
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class CountDownLatchDemo3 {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch begin = new CountDownLatch(1);
+
+        CountDownLatch end = new CountDownLatch(5);
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 5; i++) {
+            final int no = i + 1;
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("No." + no + "准备完毕，等待发令枪");
+                    try {
+                        begin.await();
+                        System.out.println("No." + no + "开始跑步了");
+                        Thread.sleep((long) (Math.random() * 10000));
+                        System.out.println("No." + no + "跑到终点了");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        end.countDown();
+                    }
+                }
+            };
+            service.submit(runnable);
+        }
+        //裁判员检查发令枪...
+        Thread.sleep(5000);
+        System.out.println("发令枪响，比赛开始！");
+        begin.countDown();
+
+        end.await();
+        System.out.println("所有人到达终点，比赛结束");
+    }
+}
