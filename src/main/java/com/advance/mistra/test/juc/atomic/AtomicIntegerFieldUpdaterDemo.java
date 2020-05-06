44c0b81a03845ff09687186d28061a0e93d5801a
wangrui <wrmistra@gmail.com>
2020-05-06 22:12:40 +0800

    Default Changelist

---
+++ src/main/java/com/advance/mistra/test/juc/atomic/AtomicIntegerFieldUpdaterDemo.java
@@ -0,0 +1,47 @@
+package com.advance.mistra.test.juc.atomic;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/4/5 18:25
+ * @ Description: AtomicIntegerFieldUpdater的用法
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class AtomicIntegerFieldUpdaterDemo implements Runnable{
+
+    static Candidate tom;
+    static Candidate peter;
+
+    public static AtomicIntegerFieldUpdater<Candidate> scoreUpdater = AtomicIntegerFieldUpdater
+            .newUpdater(Candidate.class, "score");
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            peter.score++;
+            scoreUpdater.getAndIncrement(tom);
+        }
+    }
+
+    public static class Candidate {
+        volatile int score;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        tom=new Candidate();
+        peter=new Candidate();
+        AtomicIntegerFieldUpdaterDemo r = new AtomicIntegerFieldUpdaterDemo();
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("普通变量："+peter.score);
+        System.out.println("升级后的结果"+ tom.score);
+    }
+}
