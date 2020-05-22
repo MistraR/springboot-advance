ffc11cc8097027842b0b6cdfdb2414679207c911
wangrui <wrmistra@gmail.com>
2020-05-22 21:37:26 +0800

    Default Changelist

---
+++ src/main/java/com/advance/mistra/test/juc/cas/ImitateCAS.java
@@ -0,0 +1,40 @@
+package com.advance.mistra.test.juc.cas;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/4/6 12:55
+ * @ Description: 模拟CAS操作
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class ImitateCAS implements Runnable{
+
+    private volatile int value;
+
+    public synchronized int compareAndSwap(int expectedValue, int newValue) {
+        int oldValue = value;
+        if (oldValue == expectedValue) {
+            value = newValue;
+        }
+        return oldValue;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ImitateCAS r = new ImitateCAS();
+        r.value = 0;
+        Thread t1 = new Thread(r,"Thread 1");
+        Thread t2 = new Thread(r,"Thread 2");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(r.value);
+    }
+
+    @Override
+    public void run() {
+        compareAndSwap(0, 1);
+    }
+}
