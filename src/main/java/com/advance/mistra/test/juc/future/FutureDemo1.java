d3e24320b978b3214def17a90ffe3792af72562b
wangrui <wrmistra@gmail.com>
2020-05-24 10:39:00 +0800

    Default Changelist

---
+++ src/main/java/com/advance/mistra/test/juc/future/FutureDemo1.java
@@ -0,0 +1,40 @@
+package com.advance.mistra.test.juc.future;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/3/29 10:38
+ * @ Description: 演示Future的用法
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class FutureDemo1 {
+
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        Future<Integer> future = service.submit(new CallableTask());
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        service.shutdown();
+    }
+
+    static class CallableTask implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws Exception {
+            Thread.sleep(3000);
+            return new Random().nextInt();
+        }
+    }
+
+}
