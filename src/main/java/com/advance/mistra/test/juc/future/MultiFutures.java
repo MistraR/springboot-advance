22bebd4895d8010c214125c9297fd990f5362e74
wangrui <wrmistra@gmail.com>
2020-05-25 20:28:12 +0800

    Default Changelist

---
+++ src/main/java/com/advance/mistra/test/juc/future/MultiFutures.java
@@ -0,0 +1,47 @@
+package com.advance.mistra.test.juc.future;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/3/29 10:50
+ * @ Description: 演示批量提交任务时，用List来批量接收结果
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class MultiFutures {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(20);
+        ArrayList<Future> futures = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            Future<Integer> future = service.submit(new CallableTask());
+            futures.add(future);
+        }
+        //Thread.sleep(5000);
+        for (int i = 0; i < 20; i++) {
+            Future<Integer> future = futures.get(i);
+            try {
+                Integer integer = future.get();
+                System.out.println(integer);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class CallableTask implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws Exception {
+            Thread.sleep(3000);
+            return new Random().nextInt();
+        }
+    }
+}
