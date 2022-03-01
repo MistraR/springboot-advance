c304d0f78b5c126a0a634b29dbaff2bf22c020c7
王瑞 <mistra@fiture.com>
2022-03-01 13:59:37 +0800

    redis redisson

--- src/main/java/com/advance/mistra/plugin/redis/RedisService.java
+++ src/main/java/com/advance/mistra/plugin/redis/RedisService.java
@@ -1,12 +1,16 @@
 package com.advance.mistra.plugin.redis;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
+ * 分布式锁示例
+ *
  * @author mistra@future.com
  * @date 2022/3/1
  */
+@Slf4j
 @Component
 public class RedisService {
 
@@ -17,7 +21,7 @@ public class RedisService {
         String key = String.format("MISTRA:assignUserCommunity:%s", "15000001");
         // 获取到锁之后执行业务逻辑
         reentrantLockUtil.lockThenExecute(key, () -> {
-
+            log.info("执行业务逻辑！");
         });
     }
 }
