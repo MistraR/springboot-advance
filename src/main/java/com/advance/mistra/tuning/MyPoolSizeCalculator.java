e4b2ad742cf41b41699407bbbdfede644efb1f62
MistraR <842404548@qq.com>
2023-02-28 12:40:14 +0800

    feat(*): 工具类计算线程池最佳参数

--- src/main/java/com/advance/mistra/tuning/MyPoolSizeCalculator.java
+++ src/main/java/com/advance/mistra/tuning/MyPoolSizeCalculator.java
@@ -13,6 +13,22 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class MyPoolSizeCalculator extends PoolSizeCalculator {
 
+    /**
+     * 运行结果
+     * Target queue memory usage (bytes): 100000 期望blockingQueue最大占用的内存大小
+     * createTask() produced com.advance.mistra.tuning.MyPoolSizeCalculator$AsynchronousTask which took 40 bytes in a queue
+     * Formula: 100000 / 40
+     * * Recommended queue capacity (bytes): 2500 // 推荐队列容量大小
+     * Number of CPU: 16 // 当前机器CPU核心数
+     * Target utilization: 1 // 期望CPU利用率
+     * Elapsed time (nanos): 3000000000
+     * Compute time (nanos): 3015625000
+     * Wait time (nanos): -15625000
+     * Formula: 16 * 1 * (1 + -15625000 / 3015625000) // 计算公式
+     * * Optimal thread count: 16 // 推荐最佳线程数
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         MyPoolSizeCalculator calculator = new MyPoolSizeCalculator();
         // 第一个参数：CPU目标利用率 第二个参数 blockingQueue占用的内存大小 byte
@@ -41,7 +57,7 @@ public class MyPoolSizeCalculator extends PoolSizeCalculator {
     class AsynchronousTask implements Runnable {
         @Override
         public void run() {
-            System.out.println(Thread.currentThread().getName());
+//            System.out.println(Thread.currentThread().getName());
         }
     }
 }
